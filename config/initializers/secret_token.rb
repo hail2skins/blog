b5a08f73bb5b35a64c09c2e0a3ea22430caacfe4
Art Mills <art@hamcois.com>
2013-05-04 14:13:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '8919cb6ed8ae518bebd2dba945799923958098538fe2f49bd51992d2277d597e39087beb5f51c2e3a4404e2098e476fabddade3742c3c291330f86fadb7eb5f2'
